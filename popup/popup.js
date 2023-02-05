7de201edcea7d1f99c0035ccadc480e9ef81fcd1
1option <mahagon.here@gmail.com>
2023-02-05 11:43:41 +0300

    Updated 1.2

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,5 @@ let authButton = document.querySelector("button");
 authButton.addEventListener("click", invokeAuthorization);
 
 function invokeAuthorization() {
-    let currentUrl = window.location.href;
-    chrome.runtime.sendMessage({ type: "login", currentUrl });
+    chrome.runtime.sendMessage({ type: "login" });
 }
