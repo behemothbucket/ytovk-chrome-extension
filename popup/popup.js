69eca9832f2d4d9e4a77acd65a158fd6af4bcfea
1option <mahagon.here@gmail.com>
2023-02-06 00:43:53 +0300

    Updated 1.3

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,12 @@
-let authButton = document.querySelector("button");
-authButton.addEventListener("click", invokeAuthorization);
+let authButton = document.querySelector(".button");
+let buttonClear = document.querySelector(".button_clear");
+let inputs = document.querySelectorAll(".input");
 
-function invokeAuthorization() {
-    chrome.runtime.sendMessage({ type: "login" });
-}
+buttonClear.addEventListener("click", () => {
+	for (const input of inputs) input.value = "";
+});
+
+// authButton.addEventListener("click", () => {
+// 	chrome.runtime.sendMessage({ type: "login" });
+// });
+
