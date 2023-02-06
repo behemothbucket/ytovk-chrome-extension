db13018d0c9a6fad865b369a2b02312543d0dfbf
1option <mahagon.here@gmail.com>
2023-02-06 17:34:50 +0300

    Updated 1.4

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,66 @@
-let authButton = document.querySelector(".button");
+let authButton = document.querySelector(".button_login");
 let buttonClear = document.querySelector(".button_clear");
+let buttonDownload = document.querySelector(".button_download");
 let inputs = document.querySelectorAll(".input");
+let errorIcons = document.querySelectorAll(".icon-info-circled");
 
 buttonClear.addEventListener("click", () => {
-	for (const input of inputs) input.value = "";
+	for (const input of inputs) {
+		input.value = "";
+		input.style.borderColor = "#d1d5da";
+		input.classList.remove("invalid");
+		disableErrorIcons();
+	}
 });
 
-// authButton.addEventListener("click", () => {
-// 	chrome.runtime.sendMessage({ type: "login" });
-// });
+buttonDownload.addEventListener("click", () => {
+	let totalValidInputs = 0;
+
+	for (const input of inputs) {
+		if (!!input.value) {
+			totalValidInputs++;
+		} else {
+			input.style.borderColor = "hsl(358, 68%, 59%)";
+			input.classList.add("invalid");
+			input.nextElementSibling.style.zIndex = "0";
+		}
+	}
+
+	if (totalValidInputs === 3) alert("OK");
+
+});
+
+function disableErrorIcons() {
+	errorIcons.forEach(icon => {
+		icon.style.zIndex = "-1";
+	});
+}
+
+inputs.forEach(input => {
+	input.addEventListener("change", () => {
+		if (!!input.value) {
+			input.style.borderColor = "#d1d5da";
+			input.classList.remove("invalid");
+		}
+	});
+	input.addEventListener("click", () => {
+		input.nextElementSibling.style.zIndex = "-1";
+	});
+});
+
+
+window.addEventListener("keypress", (event) => {
+	if (event.key === "Enter") {
+		event.target.classList.add("active");
+		event.target.click();
+		setTimeout(() => {
+			event.target.classList.remove("active");
+		}, 100);
+	}
+});
+
+authButton.addEventListener("click", () => {
+	// chrome.runtime.sendMessage({ type: "login" });
+	alert("Пока что отключил...");
+});
 
